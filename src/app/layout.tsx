c2d7366d1ef3a6c78775964c3b05d23d49f07c3e
Luna Santos <lunafeyli@gmail.com>
2023-10-16 19:23:23 -0300

    :chart_with_upwards_trend: Add GA

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,9 @@ import type { Metadata } from 'next';
 // Contexts Imports
 import { ThemeProvider } from '@/components/theme-provider';
 import { SettingsProvider } from '@/contexts/settings-context';
+import Script from 'next/script';
+
+const GA_ID = 'G-P0V7XD4TFG';
 
 export const metadata: Metadata = {
   title: 'MultiQSMP',
@@ -20,6 +23,27 @@ export default function RootLayout({
   return (
     <html suppressHydrationWarning>
       <head>
+        <Script
+          async
+          src={`https://www.googletagmanager.com/gtag/js? 
+					id=${GA_ID}`}
+        ></Script>
+        <Script
+          id="google-analytics"
+          dangerouslySetInnerHTML={{
+            __html: `
+							window.dataLayer = window.dataLayer || [];
+							function gtag(){dataLayer.push(arguments);}
+							gtag('js', new Date());
+
+							gtag('config', '${GA_ID}');
+						`,
+          }}
+        ></Script>
+        <meta
+          name="google-site-verification"
+          content="QQVNDnqV_O0kGHcKhluDoGXKAkDTTP3UcMJzVhoMadQ"
+        />
         <meta
           property="og:image"
           content="https://multiqsmp-beta.vercel.app/api/og"
