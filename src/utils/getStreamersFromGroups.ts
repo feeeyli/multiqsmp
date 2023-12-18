34690e8a1d79b728917e79fe4c813d1f6ff1babc
feyli <48140698+feeeyli@users.noreply.github.com>
2023-12-18 22:36:55 +0000

    :memo: Fix purgatory groups

--- src/utils/getStreamersFromGroups.ts
+++ src/utils/getStreamersFromGroups.ts
@@ -1,12 +1,14 @@
 // Data Imports
 import { GroupType } from '@/@types/data';
-import { GROUPS } from '@/data/groups';
+import { GROUPS, PURGATORY_GROUPS } from '@/data/groups';
 
 export function getStreamersFromGroups(
   groupsNames: string[],
   customGroups: GroupType[],
 ) {
-  const mergedGroups = [...new Set([...GROUPS, ...customGroups])];
+  const mergedGroups = [
+    ...new Set([...GROUPS, ...PURGATORY_GROUPS, ...customGroups]),
+  ];
 
   const groups = mergedGroups.filter((group) =>
     groupsNames.includes(group.simpleGroupName),
