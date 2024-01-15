88d332d75522bc2991ccf86944f6ede047fa1e4b
Luna Santos <lunafeyli@gmail.com>
2024-01-15 11:21:52 -0300

    Fix getAppVariant

--- src/utils/getAppVariant.ts
+++ src/utils/getAppVariant.ts
@@ -1,8 +1,9 @@
 type AppVariants = 'qsmp' | 'frogg' | 'purgatory';
 
 export const getAppVariant = () => {
-  if (/^\/purgatory/g.test((window ?? '').location.pathname))
-    return 'purgatory';
+  if (typeof window !== 'undefined') {
+    if (/^\/purgatory/g.test(window.location.pathname)) return 'purgatory';
+  }
 
   return (process.env.NEXT_PUBLIC_APP_VARIANT as AppVariants) ?? 'qsmp';
 };
