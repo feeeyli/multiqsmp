1367ea68b0d606013672946e2d537eaf1aab5fc1
Luna Santos <lunafeyli@gmail.com>
2024-01-19 17:34:46 -0300

    .

--- src/data/skinHeads.ts
+++ src/data/skinHeads.ts
@@ -1,72 +1,4 @@
-export const SKIN_HEADS = {
-  aimsey: 'https://s.namemc.com/2d/skin/face.png?id=7c02357fff44a3ed&scale=32',
-  febatista:
-    'https://s.namemc.com/2d/skin/face.png?id=b90bf174b022669d&scale=32',
-  malena0202:
-    'https://s.namemc.com/2d/skin/face.png?id=678f70c413cc4f4f&scale=32',
-  scottonauta:
-    'https://s.namemc.com/2d/skin/face.png?id=56097d3643dc64f5&scale=32',
-  jvnq: 'https://s.namemc.com/2d/skin/face.png?id=6f0b3fefe1688f57&scale=32',
-  axozer: 'https://s.namemc.com/2d/skin/face.png?id=23f228a6d47074e3&scale=32',
-  soarinng:
-    'https://s.namemc.com/2d/skin/face.png?id=7e567a804815059f&scale=32',
-  tiba041: 'https://s.namemc.com/2d/skin/face.png?id=033d4ea3b84874b3&scale=32',
-  duxo: 'https://s.namemc.com/2d/skin/face.png?id=36ac677f09b7aabf&scale=32',
-  ljoga: 'https://s.namemc.com/2d/skin/face.png?id=8060c75162f7c77c&scale=32',
-  Sneegsnag:
-    'https://s.namemc.com/2d/skin/face.png?id=df8d2e7c1df37ae0&scale=32',
-  estailus:
-    'https://s.namemc.com/2d/skin/face.png?id=481c6f1daa852036&scale=32',
-  seapeekay:
-    'https://s.namemc.com/2d/skin/face.png?id=ab9fc351a844d81f&scale=32',
-  kennystream:
-    'https://s.namemc.com/2d/skin/face.png?id=5d417fcd129c111c&scale=32',
-  supninjaz:
-    'https://s.namemc.com/2d/skin/face.png?id=efb3a5d22f95e3b9&scale=32',
-  bastet: 'https://s.namemc.com/2d/skin/face.png?id=69b67bb41ed22260&scale=32',
-  rodezel: 'https://s.namemc.com/2d/skin/face.png?id=3513e7041e28b3b0&scale=32',
-  natalan: 'https://s.namemc.com/2d/skin/face.png?id=40736d947c74a3c1&scale=32',
-  jimmyboyyy:
-    'https://s.namemc.com/2d/skin/face.png?id=7cae6d4661869aa5&scale=32',
-  vgumiho: 'https://s.namemc.com/2d/skin/face.png?id=838a980781fb8777&scale=32',
-  crystalmolly:
-    'https://s.namemc.com/2d/skin/face.png?id=f4e964005d538881&scale=32',
-  foioguto:
-    'https://s.namemc.com/2d/skin/face.png?id=0674fde65412092a&scale=32',
-  capitangatoo:
-    'https://s.namemc.com/2d/skin/face.png?id=a4a68f76f278e6b2&scale=32',
-  g0ularte:
-    'https://s.namemc.com/2d/skin/face.png?id=5ddce0b6afa4a08c&scale=32',
-  guaxinim:
-    'https://s.namemc.com/2d/skin/face.png?id=77dfc1ab6dec4d13&scale=32',
-  gabepeixe:
-    'https://s.namemc.com/2d/skin/face.png?id=c8d611085040019e&scale=32',
-  mynthos: 'https://s.namemc.com/2d/skin/face.png?id=c20c7ad2af0d7fea&scale=32',
-  cherry_rar:
-    'https://s.namemc.com/2d/skin/face.png?id=fd076f165b98eb01&scale=32',
-  wuant: 'https://s.namemc.com/2d/skin/face.png?id=e02513c340a11e08&scale=32',
-  theguill84:
-    'https://s.namemc.com/2d/skin/face.png?id=d481f88700ce7d4e&scale=32',
-  himarublack:
-    'https://s.namemc.com/2d/skin/face.png?id=a060d937a9068d78&scale=32',
-  ethannestor:
-    'https://s.namemc.com/2d/skin/face.png?id=264500e69e202bcf&scale=32',
-  jinkiwinkki:
-    'https://s.namemc.com/2d/skin/face.png?id=0a300ede0136a613&scale=32',
-  shubble: 'https://s.namemc.com/2d/skin/face.png?id=7182234ba7a6dff2&scale=32',
-  aldo_geo:
-    'https://s.namemc.com/2d/skin/face.png?id=023bc964b5d02697&scale=32',
-  jackmanifoldtv:
-    'https://s.namemc.com/2d/skin/face.png?id=2baa5d8557afc01b&scale=32',
-  Barcagamer:
-    'https://s.namemc.com/2d/skin/face.png?id=34734fa1c9976055&scale=32',
-  brunimneets:
-    'https://s.namemc.com/2d/skin/face.png?id=69f7038575461eaa&scale=32',
-  Condifiction:
-    'https://s.namemc.com/2d/skin/face.png?id=83a9565a6d8c7054&scale=32',
-
-  /* --- */
-
+const QSMP = {
   qsmp: 'https://s.namemc.com/i/0e8e0237bb86c1f5.png',
   BadBoyHalo:
     'https://s.namemc.com/2d/skin/face.png?id=bc613ffc443664bc&scale=32',
@@ -132,4 +64,104 @@ export const SKIN_HEADS = {
     'https://s.namemc.com/2d/skin/face.png?id=107ee4beef74b01a&scale=32',
   etoiles: 'https://s.namemc.com/2d/skin/face.png?id=2d7f002a5318a329&scale=32',
   kamet0: 'https://s.namemc.com/2d/skin/face.png?id=6c7a87a98c1efed1&scale=32',
+};
+
+const PURGATORY = {
+  aimsey: 'https://s.namemc.com/2d/skin/face.png?id=7c02357fff44a3ed&scale=32',
+  febatista:
+    'https://s.namemc.com/2d/skin/face.png?id=b90bf174b022669d&scale=32',
+  malena0202:
+    'https://s.namemc.com/2d/skin/face.png?id=678f70c413cc4f4f&scale=32',
+  scottonauta:
+    'https://s.namemc.com/2d/skin/face.png?id=56097d3643dc64f5&scale=32',
+  jvnq: 'https://s.namemc.com/2d/skin/face.png?id=6f0b3fefe1688f57&scale=32',
+  axozer: 'https://s.namemc.com/2d/skin/face.png?id=23f228a6d47074e3&scale=32',
+  soarinng:
+    'https://s.namemc.com/2d/skin/face.png?id=7e567a804815059f&scale=32',
+  tiba041: 'https://s.namemc.com/2d/skin/face.png?id=033d4ea3b84874b3&scale=32',
+  duxo: 'https://s.namemc.com/2d/skin/face.png?id=36ac677f09b7aabf&scale=32',
+  ljoga: 'https://s.namemc.com/2d/skin/face.png?id=8060c75162f7c77c&scale=32',
+  Sneegsnag:
+    'https://s.namemc.com/2d/skin/face.png?id=df8d2e7c1df37ae0&scale=32',
+  estailus:
+    'https://s.namemc.com/2d/skin/face.png?id=481c6f1daa852036&scale=32',
+  seapeekay:
+    'https://s.namemc.com/2d/skin/face.png?id=ab9fc351a844d81f&scale=32',
+  kennystream:
+    'https://s.namemc.com/2d/skin/face.png?id=5d417fcd129c111c&scale=32',
+  supninjaz:
+    'https://s.namemc.com/2d/skin/face.png?id=efb3a5d22f95e3b9&scale=32',
+  bastet: 'https://s.namemc.com/2d/skin/face.png?id=69b67bb41ed22260&scale=32',
+  rodezel: 'https://s.namemc.com/2d/skin/face.png?id=3513e7041e28b3b0&scale=32',
+  natalan: 'https://s.namemc.com/2d/skin/face.png?id=40736d947c74a3c1&scale=32',
+  jimmyboyyy:
+    'https://s.namemc.com/2d/skin/face.png?id=7cae6d4661869aa5&scale=32',
+  vgumiho: 'https://s.namemc.com/2d/skin/face.png?id=838a980781fb8777&scale=32',
+  crystalmolly:
+    'https://s.namemc.com/2d/skin/face.png?id=f4e964005d538881&scale=32',
+  foioguto:
+    'https://s.namemc.com/2d/skin/face.png?id=0674fde65412092a&scale=32',
+  capitangatoo:
+    'https://s.namemc.com/2d/skin/face.png?id=a4a68f76f278e6b2&scale=32',
+  g0ularte:
+    'https://s.namemc.com/2d/skin/face.png?id=5ddce0b6afa4a08c&scale=32',
+  guaxinim:
+    'https://s.namemc.com/2d/skin/face.png?id=77dfc1ab6dec4d13&scale=32',
+  gabepeixe:
+    'https://s.namemc.com/2d/skin/face.png?id=c8d611085040019e&scale=32',
+  mynthos: 'https://s.namemc.com/2d/skin/face.png?id=c20c7ad2af0d7fea&scale=32',
+  cherry_rar:
+    'https://s.namemc.com/2d/skin/face.png?id=fd076f165b98eb01&scale=32',
+  wuant: 'https://s.namemc.com/2d/skin/face.png?id=e02513c340a11e08&scale=32',
+  theguill84:
+    'https://s.namemc.com/2d/skin/face.png?id=d481f88700ce7d4e&scale=32',
+  himarublack:
+    'https://s.namemc.com/2d/skin/face.png?id=a060d937a9068d78&scale=32',
+  ethannestor:
+    'https://s.namemc.com/2d/skin/face.png?id=264500e69e202bcf&scale=32',
+  jinkiwinkki:
+    'https://s.namemc.com/2d/skin/face.png?id=0a300ede0136a613&scale=32',
+  shubble: 'https://s.namemc.com/2d/skin/face.png?id=7182234ba7a6dff2&scale=32',
+  aldo_geo:
+    'https://s.namemc.com/2d/skin/face.png?id=023bc964b5d02697&scale=32',
+  jackmanifoldtv:
+    'https://s.namemc.com/2d/skin/face.png?id=2baa5d8557afc01b&scale=32',
+  Barcagamer:
+    'https://s.namemc.com/2d/skin/face.png?id=34734fa1c9976055&scale=32',
+  brunimneets:
+    'https://s.namemc.com/2d/skin/face.png?id=69f7038575461eaa&scale=32',
+  Condifiction:
+    'https://s.namemc.com/2d/skin/face.png?id=83a9565a6d8c7054&scale=32',
+};
+
+const FROGG = {
+  scottonauta:
+    'https://s.namemc.com/2d/skin/face.png?id=56097d3643dc64f5&scale=32',
+  umildlive:
+    'https://s.namemc.com/2d/skin/face.png?id=a314e028cd6ea1bd&scale=32',
+  tiba041: 'https://s.namemc.com/2d/skin/face.png?id=033d4ea3b84874b3&scale=32',
+  kellerzons:
+    'https://s.namemc.com/2d/skin/face.png?id=e5e8bbaf0b451bde&scale=32',
+  ameizim: 'https://s.namemc.com/2d/skin/face.png?id=41695555a6490e1a&scale=32',
+  bastet: 'https://s.namemc.com/2d/skin/face.png?id=69b67bb41ed22260&scale=32',
+  carrasquera:
+    'https://s.namemc.com/2d/skin/face.png?id=be4d799ec02c65b6&scale=32',
+  dreasbro:
+    'https://s.namemc.com/2d/skin/face.png?id=0861351f94d33483&scale=32',
+  imrafly: 'https://s.namemc.com/2d/skin/face.png?id=dc0e2bfcbc1064d0&scale=32',
+  jinkiwinkki:
+    'https://s.namemc.com/2d/skin/face.png?id=0a300ede0136a613&scale=32',
+  kaaory: 'https://s.namemc.com/2d/skin/face.png?id=e3becc6ab6de81ac&scale=32',
+  kojjlul: 'https://s.namemc.com/2d/skin/face.png?id=04109ede50dff026&scale=32',
+  leoziinreact:
+    'https://s.namemc.com/2d/skin/face.png?id=2f7bbbfec8be9668&scale=32',
+  mynluvsx:
+    'https://s.namemc.com/2d/skin/face.png?id=cdb0dcfbac573a22&scale=32',
+  oflopi: 'https://s.namemc.com/2d/skin/face.png?id=aa65c99a2d0182ce&scale=32',
+};
+
+export const SKIN_HEADS = {
+  ...PURGATORY,
+  ...QSMP,
+  ...FROGG,
 };
