612f648cb15941856d2a6957f8160bc43611938b
Luna Santos <lunafeyli@gmail.com>
2024-01-19 16:58:44 -0300

    .

--- src/data/streamers/frogg.ts
+++ src/data/streamers/frogg.ts
@@ -6,16 +6,16 @@ export const FROGG_STREAMERS = [
       'https://static-cdn.jtvnw.net/jtv_user_pictures/1917209e-3c4e-49c2-8c09-e4e77141e1e1-profile_image-300x300.png',
   },
   {
-    display_name: 'Tiba041',
-    twitch_name: 'tiba041',
-    avatar_url:
-      'https://static-cdn.jtvnw.net/jtv_user_pictures/89dba2f2-567d-4697-840f-1c3cc863e7f0-profile_image-300x300.png',
-  },
-  {
     display_name: 'UmildLive',
     twitch_name: 'umildlive',
     avatar_url:
       'https://static-cdn.jtvnw.net/jtv_user_pictures/fa41da80-e980-4bb6-a580-bd6ea487b14f-profile_image-300x300.png',
+  },
+  {
+    display_name: 'Tiba041',
+    twitch_name: 'tiba041',
+    avatar_url:
+      'https://static-cdn.jtvnw.net/jtv_user_pictures/89dba2f2-567d-4697-840f-1c3cc863e7f0-profile_image-300x300.png',
   },
   {
     display_name: 'Kellerzons',
@@ -24,40 +24,22 @@ export const FROGG_STREAMERS = [
       'https://static-cdn.jtvnw.net/jtv_user_pictures/8ebe8da4-c266-4809-92bb-605331a29627-profile_image-300x300.png',
   },
   {
-    display_name: 'Bastet',
-    twitch_name: 'bastet',
-    avatar_url:
-      'https://static-cdn.jtvnw.net/jtv_user_pictures/fee53afc-157d-412e-8761-6f2f3dd741ef-profile_image-300x300.png',
-  },
-  {
-    display_name: 'Letsts',
-    twitch_name: 'letsts',
-    avatar_url:
-      'https://static-cdn.jtvnw.net/jtv_user_pictures/4876e2f2-92bd-460a-80af-9f45190de7c9-profile_image-300x300.png',
-  },
-  {
-    display_name: 'kaaory',
-    twitch_name: 'kaaory',
-    avatar_url:
-      'https://static-cdn.jtvnw.net/jtv_user_pictures/4faa84a8-2b12-4123-9b0e-61d471cacf8b-profile_image-300x300.png',
-  },
-  {
-    display_name: 'LeoziinReact',
-    twitch_name: 'leoziinreact',
-    avatar_url:
-      'https://static-cdn.jtvnw.net/jtv_user_pictures/edf27e31-5fdd-461d-836f-aea763eaca07-profile_image-300x300.png',
-  },
-  {
     display_name: 'Ameizim',
     twitch_name: 'ameizim',
     avatar_url:
       'https://static-cdn.jtvnw.net/jtv_user_pictures/69a4e8bc-01be-4f11-95ab-1c949932a13a-profile_image-300x300.png',
   },
   {
-    display_name: 'vitongemaplys',
-    twitch_name: 'vitongemaplys',
+    display_name: 'Bastet',
+    twitch_name: 'bastet',
     avatar_url:
-      'https://static-cdn.jtvnw.net/jtv_user_pictures/7b68f6a2-1316-47c9-98e6-4413dad16837-profile_image-300x300.jpg',
+      'https://static-cdn.jtvnw.net/jtv_user_pictures/fee53afc-157d-412e-8761-6f2f3dd741ef-profile_image-300x300.png',
+  },
+  {
+    display_name: 'Carrasquera',
+    twitch_name: 'carrasquera',
+    avatar_url:
+      'https://static-cdn.jtvnw.net/jtv_user_pictures/7b4253a0-eb8a-4f5e-8ea2-1b715c6c39b7-profile_image-300x300.png',
   },
   {
     display_name: 'DreasBro',
@@ -72,16 +54,16 @@ export const FROGG_STREAMERS = [
       'https://static-cdn.jtvnw.net/jtv_user_pictures/41d41923-67ac-4498-9df5-e4d38f46f96f-profile_image-300x300.png',
   },
   {
-    display_name: 'Carrasquera',
-    twitch_name: 'carrasquera',
+    display_name: 'jinkiwinkki',
+    twitch_name: 'jinkiwinkki',
     avatar_url:
-      'https://static-cdn.jtvnw.net/jtv_user_pictures/7b4253a0-eb8a-4f5e-8ea2-1b715c6c39b7-profile_image-300x300.png',
+      'https://static-cdn.jtvnw.net/jtv_user_pictures/e08bd224-64d0-4f1f-8473-1ff7b1dcf195-profile_image-300x300.png',
   },
   {
-    display_name: 'SnowyRaven_',
-    twitch_name: 'snowyraven_',
+    display_name: 'kaaory',
+    twitch_name: 'kaaory',
     avatar_url:
-      'https://static-cdn.jtvnw.net/jtv_user_pictures/1e6cf5bc-92bf-4e64-9212-916314091184-profile_image-300x300.png',
+      'https://static-cdn.jtvnw.net/jtv_user_pictures/4faa84a8-2b12-4123-9b0e-61d471cacf8b-profile_image-300x300.png',
   },
   {
     display_name: 'kojjlul',
@@ -90,27 +72,21 @@ export const FROGG_STREAMERS = [
       'https://static-cdn.jtvnw.net/jtv_user_pictures/031207fe-1ebb-4d1f-b406-27553e0ecb83-profile_image-300x300.png',
   },
   {
-    display_name: 'cinamorou',
-    twitch_name: 'cinamorou',
+    display_name: 'LeoziinReact',
+    twitch_name: 'leoziinreact',
     avatar_url:
-      'https://static-cdn.jtvnw.net/jtv_user_pictures/a5732bc0-d125-42a2-a7bb-c9996665709f-profile_image-300x300.png',
+      'https://static-cdn.jtvnw.net/jtv_user_pictures/edf27e31-5fdd-461d-836f-aea763eaca07-profile_image-300x300.png',
   },
   {
-    display_name: 'TocaDoFoxy',
-    twitch_name: 'tocadofoxy',
+    display_name: 'mynluvsx',
+    twitch_name: 'mynluvsx',
     avatar_url:
-      'https://static-cdn.jtvnw.net/jtv_user_pictures/6f6ef5bb-0701-45b4-9a8e-22e076886c1f-profile_image-300x300.png',
+      'https://static-cdn.jtvnw.net/jtv_user_pictures/7cee8027-2e00-4c58-924f-7bc30bf90e0c-profile_image-300x300.png',
   },
   {
     display_name: 'oflopi',
     twitch_name: 'oflopi',
     avatar_url:
       'https://static-cdn.jtvnw.net/jtv_user_pictures/23d2b93f-4e22-4724-a494-266310c8fd71-profile_image-300x300.png',
-  },
-  {
-    display_name: 'oakinoo',
-    twitch_name: 'oakinoo',
-    avatar_url:
-      'https://static-cdn.jtvnw.net/jtv_user_pictures/441d9598-0dc8-4dc9-bad0-e5b03e017ffb-profile_image-300x300.png',
   },
 ];
