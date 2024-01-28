b2836c6108a1bf11d90bdd5843203d17a85d7c21
feyli <48140698+feeeyli@users.noreply.github.com>
2024-01-28 20:58:03 -0300

    Add fehdubs to frogg streamers

--- src/data/streamers/frogg.ts
+++ src/data/streamers/frogg.ts
@@ -56,6 +56,12 @@ export const FROGG_STREAMERS = [
       'https://static-cdn.jtvnw.net/jtv_user_pictures/b2784187-3ee7-4c31-8161-59ac6fddda42-profile_image-300x300.png',
   },
   {
+    display_name: 'fehdubs',
+    twitch_name: 'fehdubs',
+    avatar_url: 
+      'https://static-cdn.jtvnw.net/jtv_user_pictures/f2614f03-9849-478f-8110-ab9adf8a8531-profile_image-300x300.png',
+  },
+  {
     display_name: 'ImRafly',
     twitch_name: 'imrafly',
     avatar_url:
